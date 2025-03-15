be6a4909332504dcc7a6a7556b38e64add6fc79e
vhywon <vicwhy@gmail.com>
2025-03-15 22:28:35 +0000

    completed module tutorial

---
+++ boolean_list.py
@@ -0,0 +1,53 @@
+# print(10 > 9)
+# print(10 == 9)
+# print(10 < 9)
+#
+# a = 200
+# b = 300
+#
+# if b > a:
+#   print("b is greater than a")
+# else:
+#   print("a is greater than b")
+#
+#   x = "Hello"
+#   y = 15
+#
+#   print(bool(x))
+#   print(bool(y))
+
+# zero_num = 0
+# print(bool(zero_num))
+
+# print(bool(["apple", "cherry", "banana"]))
+# bool(None)
+# print(bool(False))
+# print(bool(None))
+# print(bool(0))
+# print(bool(""))
+# print(bool(()))
+# print(bool([]))
+# print(bool({}))
+#
+# class myclass():
+#   def __len__(self):
+#     return 0
+#
+# myobj = myclass()
+# print(bool(myobj))
+#
+# def myFunction() :
+#   return True
+#
+# print(myFunction())
+#
+# def myFunction() :
+#   return True
+#
+# if myFunction():
+#   print("YES!")
+# else:
+#   print("NO!")
+
+# x = 200
+# print(isinstance(x, int))
---
+++ module.py
@@ -0,0 +1,27 @@
+# import mymodule
+#
+# mymodule.greeting("Jonathan")
+#
+# import mymodule
+#
+# a = mymodule.person1["age"]
+# print(a)
+#
+# import mymodule as mx
+#
+# a = mx.person1["age"]
+# print(a)
+#
+# import platform
+#
+# x = platform.system()
+# print(x)
+#
+# import platform
+#
+# x = dir(platform)
+# print(x)
+
+from mymodule import person1
+
+print (person1["age"])
